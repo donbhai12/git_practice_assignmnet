5a1acf025f95f99223537e4fc22a3b0a89aa9f15
animesh-0041 <ghoroianimesh1508@gmail.com>
2022-10-20 17:11:34 +0530

    I has been added logic of prime and palindrome task

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,10 @@
-
-for(let i=0;i<=NaN;i++){
-
-    
+let str = "naman";
+let bag = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  bag += str[i];
+}
+if (bag == str) {
+  console.log("Palindrome");
+} else {
+  console.log("Not a palindrome");
 }
